7ac540c9384d5b5bfff84568b994f5d1b6a29194
Sebastien Lefebvre - Kitpages <sebastien.lefebvre@kitpages.fr>
2012-01-12 11:20:10 +0100

    add a method "findByUserIdAndState" in OrderRepository

--- Repository/OrderRepository.php
+++ Repository/OrderRepository.php
@@ -4,4 +4,19 @@ use Doctrine\ORM\EntityRepository;
 
 class OrderRepository extends EntityRepository
 {
+    public function findByUserIdAndState($userId, $state)
+    {
+        $listOrder = $this->_em
+            ->createQuery('
+                SELECT o
+                FROM KitpagesShopBundle:Order o
+                JOIN o.invoiceUser iu
+                WHERE iu.userId = :userId
+                AND o.state = :state
+            ')
+            ->setParameter("userId", $userId)
+            ->setParameter("state", $state)
+            ->getResult();
+        return $listOrder;
+    }
 }
